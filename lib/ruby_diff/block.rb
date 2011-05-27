dac9d6093094ff0734552a419ee277311ed2f75c
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2011-05-27 13:40:10 +0530

    Extract some methods into a module.

--- lib/ruby_diff/block.rb
+++ lib/ruby_diff/block.rb
@@ -4,9 +4,37 @@ module RubyDiff
     def self.hunk; HunkBlock.new end
   end
 
-  class ChangedBlock < Block; end
+
+  module LeftAndRightLines
+    def left_lines
+      return @left_lines if @left_lines
+      parse_lines
+      @left_lines
+    end
+    
+    def right_lines
+      return @right_lines if @right_lines
+      parse_lines
+      @right_lines
+    end
+    
+    def parse_lines
+      hunk_line = self.first
+      @left_lines = []
+      @right_lines = []
+      self[1..-1].each do |line|
+        @left_lines << line  if (UnchangedLine === line) || (RemoveLine === line)
+        @right_lines << line if (UnchangedLine === line) || (AddLine === line)
+      end
+    end
+  end
+  
+  class ChangedBlock < Block
+    include LeftAndRightLines
+  end
   
   class HunkBlock < Block
+    include LeftAndRightLines
     
     def <<(line)
       return super(line) if HunkLine === line
@@ -38,28 +66,6 @@ module RubyDiff
     def right_offset; self.first.right_offset end
     def left_line_count; self.first.left_line_count end
     def right_line_count; self.first.right_line_count; end
-    
-    def left_lines
-      return @left_lines if @left_lines
-      parse_lines
-      @left_lines
-    end
-    
-    def right_lines
-      return @right_lines if @right_lines
-      parse_lines
-      @right_lines
-    end
-    
-    def parse_lines
-      hunk_line = self.first
-      @left_lines = []
-      @right_lines = []
-      self[1..-1].each do |line|
-        @left_lines << line  if (UnchangedLine === line) || (RemoveLine === line)
-        @right_lines << line if (UnchangedLine === line) || (AddLine === line)
-      end
-    end
     
   end
 
